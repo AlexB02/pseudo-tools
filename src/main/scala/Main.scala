99d78fd28f6f63c910597cc9de4b22915163a604
AlexB.02 <alexanderdb8@gmail.com>
2022-09-16 17:41:44 +0100

    feat: add main entrypoint

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,24 @@
+package tools
+
+import scala.io.Source
+import emulator.Emulator
+import compiler.Compiler
+import tools.IO
 
 object Main {
   def main(args: Array[String]): Unit = {
-    args.foreach(println(_))
+    if (args.length == 2) {
+      if (args(0) == "emulate") {
+        val bytecodeFilename = args(1)
+        val io = new IO()
+        val emulator = new Emulator(Source.fromFile(bytecodeFilename).getLines(), io)
+        emulator.emulate
+        io.generateLogFile("emulator.log")
+      } else if (args(0) == "compile") {
+        val sourceFilename = args(1)
+        val compiler = new Compiler(Source.fromFile(sourceFilename).getLines().mkString)
+        println(compiler.compile)
+      }
+    }
   }
 }
