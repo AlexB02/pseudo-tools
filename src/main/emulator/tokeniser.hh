7ecbd175237ace9fc28f79423c3f588f5d36f93a
AlexB.02 <alexanderdb8@gmail.com>
2022-12-21 00:59:07 +0000

    update Tokenise arguments

--- src/main/emulator/tokeniser.hh
+++ src/main/emulator/tokeniser.hh
@@ -2,7 +2,7 @@
 #include <string>
 #include <sstream>
 
-std::vector<std::string> Tokenise(std::string& string) {
+std::vector<std::string> Tokenise(std::string string) {
   std::vector<std::string> tokens;
   std::stringstream stream(string);
   std::string token;
